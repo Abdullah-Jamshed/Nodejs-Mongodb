9e0e972cce66aed453e514a3de5ca21036bf1fb0
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-03-11 20:27:22 +0500

    nodejs-updates

--- src/userModel.js
+++ src/userModel.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose");
 
+// Without Validation
+// const UserSchema = mongoose.Schema({
+//   name: {
+//     type: String,
+//     validate: {
+//       validator: (name) => name.length > 2,
+//       message: "Name must be longer than 2 characters",
+//     },
+//     required: [true, "Name Should be Required."],
+//   },
+//   age: Number,
+// });
+
+
+// With Validation
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
@@ -11,6 +26,8 @@ const UserSchema = mongoose.Schema({
   },
   age: Number,
 });
+
+
 
 const UserModel = mongoose.model("user", UserSchema);
 
