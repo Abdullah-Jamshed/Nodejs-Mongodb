70e0f519281a474fe1a80c4e0cc49bd3261077c4
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-04-02 19:43:29 +0500

    updates

--- src/operators.js
+++ src/operators.js
@@ -2,6 +2,9 @@ const UserModel = require("./userModel");
 
 const findUser = async () => {
   try {
+
+    // //  ==================   READ  ==================
+    
     //  // <<<<============>>>>
     // const data = await UserModel.find({name: "ahmed", age: 20})
     // // implicit logical "AND".
@@ -14,24 +17,81 @@ const findUser = async () => {
     // const data = await UserModel.distinct("age");
     // // return Array of age in collection
 
+    // // <<<<<=====COUNT======>>>>>
+
     //  // <<<<============>>>>
     // const data = await UserModel.count({ age: 23 });
     // estimation based on collection metadata
-
 
     //  // <<<<============>>>>
     // const data = await UserModel.estimatedDocumentCount();
     // estimation based on collection metadata
 
-
     //  // <<<<============>>>>
     // const data = await UserModel.countDocuments({ age: 23 });
     // alias for an aggregation pipeline - accurate coun
 
+    // // <<<<<=====COMPARISION======>>>>>
 
     //  // <<<<============>>>>
-    // const data = await UserModel.find({ age: { $gt: 18 } }); 
-    // return documents with age value greater than 18
+    // const data = await UserModel.find({ age: { $gt: 18 } });
+    // // return documents with age value greater than 18
+
+    //  // <<<<============>>>>
+    // const data = await UserModel.find({ age: { $gte: 18 } });
+    //  // return documents with age value greater than equals 18
+
+    //  // <<<<============>>>>
+    // const data = await UserModel.find({ age: { $lt: 18 } });
+    //  // return documents with age value less than 18
+
+    //  // <<<<============>>>>
+    // const data = await UserModel.find({ age: { $lte: 18 } });
+    //  // return documents with age value less than equals 18
+
+    //  // <<<<============>>>>
+    // const data = await UserModel.find({ ne: { $ne: 18 } });
+    //  // return documents with age value not equals 18
+
+    //  // <<<<============>>>>
+    // const data = await UserModel.find({ age: { $in: [20,30] } });
+    //  //return documents with age value equals to 20 or 30
+
+    //  // <<<<============>>>>
+    // const data = await UserModel.find({ age: { $nin: [20,30] } });
+    //  // return all documents with age value not equals to 20 or 30
+
+    // // <<<<<=====LOGICAL======>>>>>
+
+    // <<<<============>>>>
+    // const data = await UserModel.find({ name: { $not: { $eq: "ahmed" } } });
+    // // SAMEAS
+    // const data = await UserModel.find({ name: { $ne: "ahmed" } });
+    // // return all documents with name value not equal ahmed
+
+    // <<<<============>>>>
+    // const data = await UserModel.find({ $or: [{ age: 20 }, { age: 30 }] });
+    // // SAMEAS
+    // const data = await UserModel.find({ age: { $in: [20,30] } });
+    // // return documents with age value equals to 20 or 30
+
+    // <<<<============>>>>
+    // const data = await UserModel.find({ $nor: [{ age: 20 }, { age: 23 }] });
+    // // SAMEAS
+    // const data = await UserModel.find({ age: { $nin: [20,23] } });
+    //  // return all documents with age value not equals to 20 or 30
+
+    // // <<<<============>>>>
+    // const data = await UserModel.find({ $and: [{ name: "ahmed" }, { age: 20 }] });
+    // // SAMEAS
+    // const data = await UserModel.find({ name: "ahmed", age: 20 });
+    //  // return all documents with name =to ahmed  and age =to 20
+
+    // // <<<<============>>>>
+    // const data = await UserModel.find({ $and: [{ $or: [{ name: "ahmed" }, { name: "fahad" }] }, { $or: [{ age: 20 }, { age: 23 }] }] });
+    // // SAMEAS
+    // const data = await UserModel.find({ $and: [{ name: { $in: ["ahmed", "fahad"] } }, { age: { $in: [20, 23] } }] });
+    //  return all documents with name =to ahmed OR fahad  AND  age =to 20 OR 23
 
     //  // <<<<============>>>>
     // const data = await UserModel.find({ $and: [{ age: { $ne: 20 } }, { age: { $exists: true } }] });
@@ -51,6 +111,61 @@ const findUser = async () => {
     // // This query will select all documents where: the age field value equals 20 or 2, and
     // // the age field value is less than 30.
     // // This query cannot be constructed using an implicit AND operation, because it uses the $or operator more than once.
+
+    // // <<<<<=====ELEMENTS======>>>>>
+
+    //  // <<<<============>>>>
+    // const data = await UserModel.find({ age: { $exists: true } });
+    //  // return all documents that contain field age
+
+    //  // <<<<============>>>>
+    
+// ||   Double                        |    1    |       "double"                 |     
+// ||   String                        |    2    |       "string"                 |                      
+// ||   Object                        |    3    |       "object"                 |                      
+// ||   Array                         |    4    |       "array" Binary           |                      
+// ||   data                          |    5    |       "binData"                |                        
+// ||   Undefined                     |    6    |       "undefined"              |      Deprecated.                       
+// ||   ObjectId                      |    7    |       "objectId"               |
+// ||   Boolean                       |    8    |       "bool"                   |
+// ||   Date                          |    9    |       "date"                   |
+// ||   Null                          |   10    |       "null"                   |   
+// ||   Regular Expression            |   11    |       "regex"                  |
+// ||   DBPointer                     |   12    |       "dbPointer"              |      Deprecated.
+// ||   JavaScript                    |   13    |       "javascript"             |     
+// ||   Symbol                        |   14    |       "symbol"                 |      Deprecated.
+// ||   JavaScript code with scope    |   15    |       "javascriptWithScope"    |      Deprecated in MongoDB 4.4.
+// ||   32-bit integer|               |   16    |       "int"                    |
+// ||   Timestamp                     |   17    |       "timestamp"              |      
+// ||   64-bit integer                |   18    |       "long"                   |       
+// ||   Decimal128                    |   19    |       "decimal"                |      New in version  3.4.
+// ||   Min key                       |   -1    |       "minKey"                 | 
+// ||   Maxkey                        |  127    |       "maxKey"                 |
+
+    // const data = await UserModel.find({ age: { $type: 16 } });
+    const data = await UserModel.find({ age: { $type: "int" } });
+    //  // return all documents that contain field age with type of number
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // //  ==================   UPDATE  ==================
+
+
+
+
+
+
+
 
     return data;
   } catch {
